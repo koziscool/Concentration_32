f33519369e529638acf02390fe7cba91856b7117
John Kosmicke <kozjob@yahoo.com>
2019-04-17 08:29:10 -0700

    update

--- cardmatcher.js
+++ cardmatcher.js
@@ -1,3 +1,31 @@
 
+matcherController = {
+  init: function( size ) {
+    
+  },
+};
 
+matcherView = {
+  updateGameView: function(  ) {
+    
+  },
+};
+
+
+
+$(document).ready( function() {
+  $("#grid-size").focus();
+  $("#grid-size-button").click( function(e){
+    e.preventDefault();
+    var size = $("#grid-size").val();
+    if( Math.floor(size/2) === size/2 ){
+      matcherController.init();
+      matcherView.updateGameView();
+      $("#grid-size-form").hide();
+    } else {
+      $("#grid-size").val( "Bad input, try again" );
+
+    }
+  });
+});
 
